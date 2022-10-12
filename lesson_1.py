414d7fd6a259237501d934a12415a375ddf6dfa7
Elena Shvedova <111299782+E1ena-SH@users.noreply.github.com>
2022-10-12 21:48:58 +0500

    Create lesson_1.py

---
+++ lesson_1.py
@@ -0,0 +1,24 @@
+# print('Hello world')
+
+value = None
+a = 123
+b = 1.23
+# print(type(value))
+# print(type(a))
+# print(type(b))
+value = 321
+# print(type(value))
+# s = 'qwerty'
+# print(s) # вывод строки
+# print(a,'-', b,'-', s)
+# print('{} - {} - {}'.format(a, b, s))
+# print(f'{a} - {b} - {s}')
+# print('{1} - {2} - {0}'.format(a, b, s))
+# f = True
+# g = False
+# print(f, g)
+
+list = ['1', '2', '3']
+col = ['hello', 1,2,4.5, True]
+print(list)
+print(col)
